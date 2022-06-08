d30deb6f2c4a4362efc13cf00a4da002638c1628
Pankaj Jha <mail@pankajjha.me>
2022-06-08 10:50:25 +0530

    Updated to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="newscleaner",
-    version="0.0.4",
+    version="0.0.5",
     author="Pankaj Jha",
     author_email="mail@pankajjha.me",
     description="Package to clean up waste postfix from articles",
